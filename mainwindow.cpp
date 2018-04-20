734c6e3ac45c1f46a7fde79ec1e8c848943c9fc9
alidouiyek <alidouiyek@gmail.com>
2018-04-20 20:28:56 +0200

    Rewrote most of the class. Now GLWidget is created when the video is opened, instead of when the app is launched.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,105 +1,145 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    gw=ui->m_glwidget;
+
+    connect(&grabFramesTimer, SIGNAL(timeout()), this, SLOT(on_grabFrame()));
+    connect(&updateGuiTimer, SIGNAL(timeout()), this, SLOT(on_updateGui()));
+    updateGuiTimer.start(500);
 }
 
-//Read frames and update UI
-void MainWindow::timerEvent(QTimerEvent *)
+//Clean memory
+MainWindow::~MainWindow()
 {
+    if(videoWidget!=NULL)
+        delete videoWidget;
+
+    delete ui;
+}
+
+//Open a window to browse to the video source and then start reading the video
+void MainWindow::on_browseButton_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Select video", QDir::homePath(), "Video files (*.*)");
+    std::string videoPath=fileName.toStdString();
+
+    if(videoPath=="")  //if the user pressed "Cancel"
+        return;
+
+    if(!cap.open(videoPath)){
+        QMessageBox::information (0, "Error", QString("Invalid path"));
+                  return;
+    }
+    cap.read(imgSrc);
+    frameDurationMs=1000/cap.get(CV_CAP_PROP_FPS);
+
+    if(videoWidget!=NULL)
+        delete videoWidget;
+
+    videoWidget=new GLWidget(ui->viewerGroupBox, imgSrc.size());
+    ui->viewerGroupBox->layout()->addWidget(videoWidget);
+    videoWidget->show();
+    ellapsedTimeMs=0;
+    on_satSlider_sliderMoved(ui->satSlider->value());
+
+    grabFramesTimer.stop();
+    grabFramesTimer.start(frameDurationMs);
+}
+
+//Update progress bar & time  label
+void MainWindow::on_updateGui()
+{
+    if(!cap.isOpened())
+        return;
+
+    //Time Label
+    int minutes=ellapsedTimeMs/60000;
+    int seconds=((int)ellapsedTimeMs%60000)/1000;
+    QString textTime=((minutes<10)?"0":"")+QString::number(minutes)+":"+((seconds<10)?"0":"")+QString::number(seconds);
+    ui->timeLabel->setText(textTime);
+
+    //Progress bar
+    int videoLenght=cap.get(CV_CAP_PROP_FRAME_COUNT)*frameDurationMs;
+    int progress= ui->progressSlider->maximum()* ellapsedTimeMs/videoLenght;
+    ui->progressSlider->setValue(progress);
+}
+
+//Reads new frames
+void MainWindow::on_grabFrame()
+    {
     if(!cap.isOpened())
         return;
 
     if(!cap.read(imgSrc))
     {
-        milliSec=0;
-        cap.set(CV_CAP_PROP_POS_MSEC,milliSec);
+        ellapsedTimeMs=0;
+        cap.set(CV_CAP_PROP_POS_MSEC,ellapsedTimeMs);
     }
-    milliSec+=30;
-    gw->updateImage(imgSrc.ptr());
+    ellapsedTimeMs+=frameDurationMs;
 
-    if(milliSec%1000 <=30)
-    {
-        //Time Label
-        int min=milliSec/60000;
-        int sec=(milliSec%60000)/1000;
-        QString textTime=((min<10)?"0":"")+QString::number(min)+":"+((sec<10)?"0":"")+QString::number(sec);
-        ui->timeLabel->setText(textTime);
-
-        //Progress bar
-        float lenght=cap.get(CV_CAP_PROP_FRAME_COUNT)*30;
-        float current= 100* milliSec/lenght;
-        ui->progressSlider->setValue(current);
-    }
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
-//Open video
-void MainWindow::on_openButton_clicked()
-{
-    std::string path=ui->pathTextBox->toPlainText().toStdString();
-    if(path=="")
-        return;
-
-    if(cap.isOpened())
-        cap.release();
-
-    cap.open(path);
-    cap.read(imgSrc);
-    gw->setImageSize(imgSrc.size());
-    milliSec=0;
-
-    startTimer(30);
+    if(imgSrc.data)
+        videoWidget->updateImage(imgSrc.ptr());
 }
 
 //Change Saturation
-void MainWindow::on_satSlider_valueChanged(int value)
+void MainWindow::on_satSlider_sliderMoved(int position)
 {
-    float valf=(float)value/10;
-    ui->satLabel->setText("Saturation: "+QString::number(valf));
-    gw->setSaturation(valf);
+    if(videoWidget!=NULL){
+        float val=(float)position/10;
+        ui->satLabel->setText("Saturation: "+QString::number(val));
+        videoWidget->setSaturation(val);
+    }
+    else
+        ui->satSlider->setValue(10);
 }
 
 //Rewind 10sec
 void MainWindow::on_backButton_clicked()
 {
-    milliSec-=10000;
-    if(milliSec<0)
-        milliSec=0;
-
-    cap.set(CV_CAP_PROP_POS_MSEC,milliSec);
-
-    std::cout<<milliSec<<std::endl;
+    if(cap.isOpened())
+    {
+        ellapsedTimeMs-=10000;
+        if(ellapsedTimeMs<0)
+            ellapsedTimeMs=0;
+        cap.set(CV_CAP_PROP_POS_MSEC,ellapsedTimeMs);
+    }
 }
 
 //Go forward 10sec
 void MainWindow::on_forwardButton_clicked()
 {
-    milliSec+=10000;
-    cap.set(CV_CAP_PROP_POS_MSEC,milliSec);
-    std::cout<<milliSec<<std::endl;
+    if(cap.isOpened())
+    {
+        ellapsedTimeMs+=10000;
+        cap.set(CV_CAP_PROP_POS_MSEC,ellapsedTimeMs);
+    }
+}
+
+//Toggle Pause / Play
+void MainWindow::on_pauseButton_clicked()
+{
+    if(grabFramesTimer.isActive())
+        grabFramesTimer.stop();
+    else
+        grabFramesTimer.start();
+
 }
 
 //Scroll video with progress bar
-void MainWindow::on_progressSlider_valueChanged(int value)
+void MainWindow::on_progressSlider_sliderMoved(int position)
 {
-    if(!cap.isOpened())
+    if(cap.isOpened())
     {
-        milliSec=0;
+        float lenght=cap.get(CV_CAP_PROP_FRAME_COUNT)*frameDurationMs;
+        ellapsedTimeMs= position*lenght/ui->progressSlider->maximum();
+        cap.set(CV_CAP_PROP_POS_MSEC,ellapsedTimeMs);
+    }
+    else
+    {
+        ellapsedTimeMs=0;
         ui->progressSlider->setValue(0);
         return;
     }
-
-    float lenght=cap.get(CV_CAP_PROP_FRAME_COUNT)*30;
-    milliSec= value*lenght/100;
-    cap.set(CV_CAP_PROP_POS_MSEC,milliSec);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QDir>
 #include <QTimer>
 #include <opencv2/opencv.hpp>
 #include <cuda_gl_interop.h>
@@ -20,26 +23,32 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-    void timerEvent(QTimerEvent *);
     void updateTimeLabel();
 
 private slots:
-    void on_satSlider_valueChanged(int value);
-
-    void on_openButton_clicked();
+    void on_browseButton_clicked();
     void on_backButton_clicked();
     void on_forwardButton_clicked();
+    void on_pauseButton_clicked();
 
-    void on_progressSlider_valueChanged(int value);
+    void on_progressSlider_sliderMoved(int position);
+    void on_satSlider_sliderMoved(int position);
+
+    void on_grabFrame();
+    void on_updateGui();
+
 
 private:
     Ui::MainWindow *ui;
-    GLWidget* gw;
+    GLWidget* videoWidget=NULL;
+
     cv::VideoCapture cap;
     cv::Mat imgSrc;
-    QTimer timer;
-    int milliSec=0;
+    QTimer grabFramesTimer;
+    QTimer updateGuiTimer;
 
+    float ellapsedTimeMs=0;
+    float frameDurationMs=55;
 };
 
 #endif // MAINWINDOW_H
